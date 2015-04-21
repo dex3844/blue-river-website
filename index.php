01bcd3774710db221f10cbda2073372eaa2a9702
dex3844 <dex3844@yahoo.com>
2015-04-21 16:01:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Blue River Academy</title>
+
+		<link rel="stylesheet" href="css/foundation.css">
+		<!-- This is how you would link your custom stylesheet -->
+		<link rel="stylesheet" href="css/main.css">
+		<script src="js/vendor/modernizr.js"></script>
+	<head>
+	<body>
+		<div class="row">
+			<div class="small-12 columns">
+				<h1>Welcome to Blue River Academy</h1>
+			</div>
+		</div>
+
+	</body>
+</html>
